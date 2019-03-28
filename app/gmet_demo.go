8912d9faf34f983202c826b6fa679d8f8ab0784f
Leon <leon@mobvista.com>
2019-03-28 19:10:11 +0800

    update demo

--- app/gmet_demo.go
+++ app/gmet_demo.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	. "github.com/easierway/g_met"
-	"log"
-	"log/syslog"
 	"math/rand"
 	"time"
 
@@ -11,20 +9,13 @@ import (
 
 // The following example is to demonstrate how to use GMet.
 func main() {
-	sysLog, err := syslog.Dial("tcp", "localhost:514",
-		syslog.LOG_WARNING|syslog.LOG_DAEMON, "demo tag")
-	if err != nil {
-		log.Print(err)
-	}
-
 	// create GMet instance by given default writer and the formatter
 	gmet := CreateGMetInstanceByDefault("seelog.xml")
 	for i := 0; i < 100; i++ {
 		gmet.Send(Metric("input_bytes", rand.Intn(100)),
 			Metric("output_bytes", rand.Intn(100)))
 		gmet.Flush() // in your real case, DON'T flush for each sending.
-		sysLog.Info("And this is a daemon emergency with demotag.")
 		// For seelog writer, the auto-flushing can be set in the log configuration
-		time.Sleep(time.Second * 1)
+		time.Sleep(time.Millisecond * 500)
 	}
 }
