f6ac8ffc99dc7c4380d6744a5b7482665be2bae8
leon <leon@mobvista.com>
2019-04-22 11:52:55 +0800

    add counter test

--- registry.go
+++ registry.go
@@ -29,7 +29,7 @@ type Registry interface {
 	Get(string) interface{}
 
 	// GetAll metrics in the Registry.
-	// GetAll() map[string]map[string]interface{}
+	GetAll() map[string]map[string]interface{}
 
 	// Gets an existing metric or registers the given one.
 	// The interface can be the metric to register if not found in registry,
@@ -38,9 +38,6 @@ type Registry interface {
 
 	// Register the given metric under the given name.
 	Register(string, interface{}) error
-
-	// Run all registered healthchecks.
-	// RunHealthchecks()
 
 	// Unregister the metric with the given name.
 	Unregister(string)
@@ -109,77 +106,19 @@ func (r *StandardRegistry) Register(name string, i interface{}) error {
 	return r.register(name, i)
 }
 
-// Run all registered healthchecks.
-// func (r *StandardRegistry) RunHealthchecks() {
-// 	r.mutex.RLock()
-// 	defer r.mutex.RUnlock()
-// 	for _, i := range r.metrics {
-// 		if h, ok := i.(Healthcheck); ok {
-// 			h.Check()
-// 		}
-// 	}
-// }
-
 // GetAll metrics in the Registry
-// func (r *StandardRegistry) GetAll() map[string]map[string]interface{} {
-// 	data := make(map[string]map[string]interface{})
-// 	r.Each(func(name string, i interface{}) {
-// 		values := make(map[string]interface{})
-// 		switch metric := i.(type) {
-// 		case Counter:
-// 			values["count"] = metric.Count()
-// 		case Gauge:
-// 			values["value"] = metric.Value()
-// 		case GaugeFloat64:
-// 			values["value"] = metric.Value()
-// 		case Healthcheck:
-// 			values["error"] = nil
-// 			metric.Check()
-// 			if err := metric.Error(); nil != err {
-// 				values["error"] = metric.Error().Error()
-// 			}
-// 		case Histogram:
-// 			h := metric.Snapshot()
-// 			ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-// 			values["count"] = h.Count()
-// 			values["min"] = h.Min()
-// 			values["max"] = h.Max()
-// 			values["mean"] = h.Mean()
-// 			values["stddev"] = h.StdDev()
-// 			values["median"] = ps[0]
-// 			values["75%"] = ps[1]
-// 			values["95%"] = ps[2]
-// 			values["99%"] = ps[3]
-// 			values["99.9%"] = ps[4]
-// 		case Meter:
-// 			m := metric.Snapshot()
-// 			values["count"] = m.Count()
-// 			values["1m.rate"] = m.Rate1()
-// 			values["5m.rate"] = m.Rate5()
-// 			values["15m.rate"] = m.Rate15()
-// 			values["mean.rate"] = m.RateMean()
-// 		case Timer:
-// 			t := metric.Snapshot()
-// 			ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-// 			values["count"] = t.Count()
-// 			values["min"] = t.Min()
-// 			values["max"] = t.Max()
-// 			values["mean"] = t.Mean()
-// 			values["stddev"] = t.StdDev()
-// 			values["median"] = ps[0]
-// 			values["75%"] = ps[1]
-// 			values["95%"] = ps[2]
-// 			values["99%"] = ps[3]
-// 			values["99.9%"] = ps[4]
-// 			values["1m.rate"] = t.Rate1()
-// 			values["5m.rate"] = t.Rate5()
-// 			values["15m.rate"] = t.Rate15()
-// 			values["mean.rate"] = t.RateMean()
-// 		}
-// 		data[name] = values
-// 	})
-// 	return data
-// }
+func (r *StandardRegistry) GetAll() map[string]map[string]interface{} {
+	data := make(map[string]map[string]interface{})
+	r.Each(func(name string, i interface{}) {
+		values := make(map[string]interface{})
+		switch metric := i.(type) {
+		case Counter:
+			values["count"] = metric.Count()
+		}
+		data[name] = values
+	})
+	return data
+}
 
 // Unregister the metric with the given name.
 func (r *StandardRegistry) Unregister(name string) {
@@ -193,7 +132,7 @@ func (r *StandardRegistry) Unregister(name string) {
 func (r *StandardRegistry) UnregisterAll() {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
-	for name, _ := range r.metrics {
+	for name := range r.metrics {
 		r.stop(name)
 		delete(r.metrics, name)
 	}
@@ -203,10 +142,10 @@ func (r *StandardRegistry) register(name string, i interface{}) error {
 	if _, ok := r.metrics[name]; ok {
 		return DuplicateMetric(name)
 	}
-	// switch i.(type) {
-	// case Counter, Gauge, GaugeFloat64, Healthcheck, Histogram, Meter, Timer:
-	// 	r.metrics[name] = i
-	// }
+	switch i.(type) {
+	case Counter:
+		r.metrics[name] = i
+	}
 	return nil
 }
 
@@ -298,15 +237,10 @@ func (r *PrefixedRegistry) Register(name string, metric interface{}) error {
 	return r.underlying.Register(realName, metric)
 }
 
-// Run all registered healthchecks.
-// func (r *PrefixedRegistry) RunHealthchecks() {
-// 	r.underlying.RunHealthchecks()
-// }
-
 // GetAll metrics in the Registry
-// func (r *PrefixedRegistry) GetAll() map[string]map[string]interface{} {
-// 	return r.underlying.GetAll()
-// }
+func (r *PrefixedRegistry) GetAll() map[string]map[string]interface{} {
+	return r.underlying.GetAll()
+}
 
 // Unregister the metric with the given name. The name will be prefixed.
 func (r *PrefixedRegistry) Unregister(name string) {
@@ -350,11 +284,6 @@ func MustRegister(name string, i interface{}) {
 		panic(err)
 	}
 }
-
-// Run all registered healthchecks.
-// func RunHealthchecks() {
-// 	DefaultRegistry.RunHealthchecks()
-// }
 
 // Unregister the metric with the given name.
 func Unregister(name string) {
